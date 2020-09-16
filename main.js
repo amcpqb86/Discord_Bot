5a5f3950a6afea870d8fa7aa20e2b7811746fffd
Amelien BERNARD <amcpqb86@gmail.com>
2020-09-16 20:41:30 +0200

    retry

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const {Client} = require('discord.js');
+const client = new Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.channel.send('Pong!');
+  if (msg.content === '§salut') {
+    msg.channel.send('Je suis le bot !');
   }
 });
 
-client.login('NzU1NTE4NjQ0Mjk2ODc2MTAz.X2EdkQ.-lxpLSMzDuvE822Nx5sKh9m-cPk');
+client.login('NzU1NTE4NjQ0Mjk2ODc2MTAz.X2EdkQ.wvWRIW95Yaz7MhC-p7jcMRw_CN8');
